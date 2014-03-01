38a630443b8e525c51c5d2b510e47baf1a776065
Benjamin Russell <benrr101@csh.rit.edu>
2014-03-01 15:25:37 -0500

    Functionality for loading track library and rudimentary displaying of it

--- scripts/app2.js
+++ scripts/app2.js
@@ -12,12 +12,50 @@ var apiKey = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08";
 var serverAddress = "https://localhost:8080/";
 
 var baseAjaxParams = {
-    cache: false,
     contentType: "application/json; charset=utf-8",
     dataType: "json",
     xhrFields: { withCredentials:true }
 };
 
+// UTILITY FUNCTIONS ///////////////////////////////////////////////////////
+/**
+ * Generates a text string representation of a track number. Formats look like:
+ * {dN}.{tN}    if disc number and track number are provided
+ * {tN}         if track number only is provided
+ * ""           if disc number only or no track number is provided
+ * @param trackNumber   int the track number of the track
+ * @param discNumber    int the disc number of the track
+ * @returns string  String representation of the disc number & track number
+ */
+function calculateTrackNumber(trackNumber, discNumber) {
+    var result = "";
+    if(trackNumber) {
+        result = String(trackNumber);
+        if(discNumber) {
+            result = String(discNumber) + "." + result;
+        }
+    }
+    return result;
+}
+
+function calculateTrackTime(time) {
+    time = parseInt(time);
+
+    // Use a divide and mod method to get hours, minutes, seconds for the track
+    var hours = Math.floor(time/3600);
+    time %= 3600;
+    var minutes = Math.floor(time/60);
+    var seconds = time % 60;
+
+    // Format for text
+    seconds = seconds < 10 ? "0" + seconds : String(seconds);
+    minutes = hours > 0 ? (minutes < 10 ? "0" + minutes : String(minutes)) : minutes;
+
+    // Put it together into a string
+    return (hours > 0 ? String(hours) + ":" : "") + minutes + ":" + seconds;
+}
+
+// VIEW MODEL //////////////////////////////////////////////////////////////
 function ViewModel() {
     var self = this;
 
@@ -29,8 +67,11 @@ function ViewModel() {
     self.loginPassword = ko.observable();       // Used for storing the password the user is logging in with.
     self.loginUsername = ko.observable();       // Used for storing the username the user is logging in with.
 
-    self.navAutoPlaylists = ko.observableArray();
-    self.navStaticPlaylists = ko.observableArray();
+    self.trackLibrary = {};                     // Used for storing all tracks.
+
+    self.navAutoPlaylists = ko.observableArray();       // Used for storing the list of auto playlists
+    self.navStaticPlaylists = ko.observableArray();     // Used for storing the list of static playlists
+    self.trackVisibleTracks = ko.observableArray();     // Used for storing the list of VISIBLE tracks in the tracks pane
 
     // ACTIONS /////////////////////////////////////////////////////////////
     self.loginSubmitLogin = function() {
@@ -47,6 +88,7 @@ function ViewModel() {
 
             self.loadAutoPlaylists();
             self.loadStaticPlaylists();
+            self.loadTrackLibrary(true);
         };
         $.ajax(params);
     };
@@ -78,6 +120,23 @@ function ViewModel() {
         }
         $.ajax(params);
     }
+
+    self.loadTrackLibrary = function(setVisible) {
+        var params = baseAjaxParams;
+        delete params.data;
+        params.type = "GET";
+        params.url = serverAddress + "tracks/";
+        params.error = function(jqXHR) { // Show error message
+            self.generalError(jqXHR.status != 0 ? jqXHR.responseJSON.Message : "Failed to lookup track library for unknown reason.");
+        };
+        params.success = function(jqXHR) { // Store the tracks in the invisible library
+            for(var i = 0; i < jqXHR.length; ++i) {
+                self.trackLibrary[jqXHR[i].Id] = jqXHR[i];
+                if(setVisible) { self.trackVisibleTracks.push(jqXHR[i]); }
+            }
+        };
+        $.ajax(params);
+    };
 }
 
 // Activates knockout.js
