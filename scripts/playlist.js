4d8a6b8acba3580a5a93f71e73e0f8add1f99a1c
Benjamin Russell <benrr101@csh.rit.edu>
2014-04-27 20:11:18 -0400

    Further refactoring to make the playlist viewmodel's actions part of the prototype, not the instance

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -6,11 +6,12 @@
  * To change this template use File | Settings | File Templates.
  */
 
+// DATA MEMBERS ////////////////////////////////////////////////////////////
 function PlaylistViewModel(type, parent) {
     var self = this;
 
-    // DATA MEMBERS ////////////////////////////////////////////////////////
     // PARENT VM BACK REFERENCE ////////////////////////////////////////////
+    //@TODO: Replace with callbacks to handle these sorts of things
     self.parent = parent;   // A reference back to the parent VM for handling errors
 
     // NON-OBSERVABLE //////////////////////////////////////////////////////
@@ -27,72 +28,84 @@ function PlaylistViewModel(type, parent) {
     self.Name = ko.observable(null);    // The name of the playlist
     self.Tracks = ko.observableArray(); // A list of GUIDs that correspond to tracks that belong to this playlist
 
-    // ACTIONS /////////////////////////////////////////////////////////////
-    // NON-AJAX ////////////////////////////////////////////////////////////
+}
 
-    // AJAX ////////////////////////////////////////////////////////////////
-    self.fetch = function(callback) {
-        // Build a request for the playlist
-        var params = getBaseAjaxParams("GET", serverAddress + self.Href);
+// ACTIONS /////////////////////////////////////////////////////////////////
+// AJAX ////////////////////////////////////////////////////////////////////
+/**
+ * Fetches the playlist from the server. Performs an optional callback when
+ * completed.
+ * @param callback (Function|null)  The optional callback to perform when the
+ *                                  playlist has been successfully loaded
+ */
+PlaylistViewModel.prototype.fetch = function(callback) {
+    var self = this;
 
-        // Add error handler
-        params.error = function(jqXHR) {
-            self.parent.generalError(jqXHR.status != 0 ? jqXHR.responseJSON.Message : "Failed to lookup playlist for unknown reason.");
-        };
+    // Build a request for the playlist
+    var params = getBaseAjaxParams("GET", serverAddress + self.Href);
 
-        // Set up the success handler based on the type of the playlist
-        if(type == "static") {
-            params.success = function(jqXHR) {
-                // Set the internal variables and mark as loaded
-                self.Tracks = ko.observableArray(jqXHR.Tracks);
-                self.Loaded = true;
-
-                if(callback !== null) {
-                    callback(self);
-                }
-            }
-        } else {
-            params.success = function(jqXHR) {
-                // Set the internal variables and mark as loaded
-                self.Tracks = ko.observable(jqXHR.Tracks);
-                self.Loaded = true;
-
-                if(callback !== null) {
-                    callback(self);
-                }
-            }
-        }
-
-        // Make the call
-        $.ajax(params);
+    // Add error handler
+    params.error = function(jqXHR) {
+        self.parent.generalError(jqXHR.status != 0 ? jqXHR.responseJSON.Message : "Failed to lookup playlist for unknown reason.");
     };
 
-    self.addTracks = function(selection) {
-        // Only run this if this is a static playlist
-        if(self.type != "static") {
-            console.log("Cannot add tracks to an auto playlist");
-            return;
+    // Set up the success handler based on the type of the playlist
+    if(self.type == "static") {
+        params.success = function(jqXHR) {
+            // Set the internal variables and mark as loaded
+            self.Tracks = ko.observableArray(jqXHR.Tracks);
+            self.Loaded = true;
+
+            if(callback !== null) {
+                callback(self);
+            }
         }
+    } else {
+        params.success = function(jqXHR) {
+            // Set the internal variables and mark as loaded
+            self.Tracks = ko.observable(jqXHR.Tracks);
+            self.Loaded = true;
 
-        // Build an ajax request for EACH selected track
-        // TODO: Do this with a single batch call when Dolomite supports it
-        selection.forEach(function(item) {
-            // Build a request to add the tracks
-            var params = getBaseAjaxParams("POST", serverAddress + self.Href);
-            params.error = params.error = function(jqXHR) {
-                self.parent.generalError(jqXHR.status != 0 ? jqXHR.responseJSON.Message : "Failed to add tracks to playlist for unknown reason.");
-            };
-            params.success = function() {
-                // If the playlist has already been loaded, then force a reload of the playlist
-                // We do a full reload since due to the async nature of the requests, the order
-                // of the playlist COULD be wrong. When the batch is supported in dolomite, this
-                // will not be a problem, and we can just tack the new track onto the end
-                self.fetch(null);
-            };
-            params.data = item.Id;
-
-            // Fire off the request
-            $.ajax(params);
-        });
+            if(callback !== null) {
+                callback(self);
+            }
+        }
     }
-}
+
+    // Make the call
+    $.ajax(params);
+};
+
+/**
+ * Adds the provided list of track guids to this static playlist.
+ * @throws  Error   Thrown if this method is called on an automatic playlist.
+ * @param selection Array<guid> An array of track guids to add to the playlist.
+ */
+PlaylistViewModel.prototype.addTracks = function(selection) {
+    // Only run this if this is a static playlist
+    if(this.type != "static") {
+        throw new Error("Cannot add tracks to an auto playlist.");
+        return;
+    }
+
+    // Build an ajax request for EACH selected track
+    // TODO: Do this with a single batch call when Dolomite supports it
+    selection.forEach(function(item) {
+        // Build a request to add the tracks
+        var params = getBaseAjaxParams("POST", serverAddress + this.Href);
+        params.error = params.error = function(jqXHR) {
+            this.parent.generalError(jqXHR.status != 0 ? jqXHR.responseJSON.Message : "Failed to add tracks to playlist for unknown reason.");
+        };
+        params.success = function() {
+            // If the playlist has already been loaded, then force a reload of the playlist
+            // We do a full reload since due to the async nature of the requests, the order
+            // of the playlist COULD be wrong. When the batch is supported in dolomite, this
+            // will not be a problem, and we can just tack the new track onto the end
+            this.fetch(null);
+        };
+        params.data = item.Id;
+
+        // Fire off the request
+        $.ajax(params);
+    });
+};
